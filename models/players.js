f771f60c35e6682dd16cb65881711e6477e8ef36
andrewredman91 <andrewredman91@gmail.com>
2022-05-11 15:52:47 +0100

    second commit

--- models/players.js
+++ models/players.js
@@ -1,15 +1,21 @@
-let Players = function (name, dateOfBirth, position){
+const Players = function (name, dateOfBirth, position){
     this.name = name;
     this.dateOfBirth = dateOfBirth;
     this.position = position;
-    this.list = []
+    this.playerList = []
+}
+let player1 = new Players("McGinn", "07/09/1990", "RB");
+let player2 = new Players("Melkerson", "01/02/2004", "ST");
+
+let playerList = []
+
+new function addPlayerToTeam() {
+
+playerList.push(player1,player2);
+return playerList[player1, player2]
 }
 
-  let players;
 
-//     beforeEach(function () {
-//         players = new Players("McGinn", "07/09/1990", "RB");
-// })
-// })
-// console.log(player1)
-module.exports = Players;
+
+
+console.log(playerList)
